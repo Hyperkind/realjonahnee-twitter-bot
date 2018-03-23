8d810200e8e665212b04eb2f7412ac5f52658a60
Jonathan Ah-Nee <jonahnee@gmail.com>
2018-03-22 19:58:19 -1000

    working on api calls

--- src/bot.js
+++ src/bot.js
@@ -3,12 +3,35 @@ const config = require('./config');
 
 const bot = new Twit(config);
 
-bot.post('statuses/update', {
-  status: 'hello world!'
-}, (err, data, response) => {
-  if(err) {
-    console.log(err)
-  } else {
-    console.log(`${data.text} tweeted!`)
-  }
-})
+// bot.post('statuses/update', {
+//   status: 'hello world!'
+// }, (err, data, response) => {
+//   if(err) {
+//     console.log(err)
+//   } else {
+//     console.log(`${data.text} tweeted!`)
+//   }
+// });
+
+// bot.get('followers/list', {
+//   screen_name: 'RealJonAhNee',
+//   count: 5
+// }, (err, data, res) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     data.users.forEach(user => {
+//       console.log(user)
+//     })
+//   }
+// })
+
+// bot.get('friendships/lookup', {
+//   screen_name: 'jlieu'
+// }, (err, data, res) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(data)
+//   }
+// })
